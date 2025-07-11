3a4778cb0d6ed151aeb1effa4f56a9141dd72f24
Emmanuel Odongo <odiwourmanu123@gmail.com>
2025-07-11 10:09:18 +0300

    updates

--- app.py
+++ app.py
@@ -954,7 +954,7 @@ def sitemap():
     sitemap_xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://bass-uzzi.onrender.com/</loc>
+    <loc>https://bishopabieroshaurimoyo.com/</loc>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
